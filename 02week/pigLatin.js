b86ec5dd1495db76ccb625bc3520928597213166
Rene Cruz <renecruzatx@outlook.com>
2018-07-28 19:43:42 -0500

    Pig Latin upload

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -8,16 +8,45 @@ const rl = readline.createInterface({
 });
 
 
-function pigLatin(word) {
+const pigLatin = (word) => {
 
-  // Your code here
+  //create vowelArray so I can check first letter of word agains this array
+  const vowelArray = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+  //this function will check if the input is a valid string. Single words only.
+  const isWordValid = (word) => {
+    return typeof word == 'string' && !(word.includes(" "));
+  }
+
+  //This function takes the first letter of the string and checks it against the vowelArray to see if the word begins with a vowel or not
+  const isFirstCharAVowel = (word) => {
+    const firstChar = word.substring(0, 1);
+    return vowelArray.includes(firstChar);
+  }
+
+  //check if input is a valid string with a function  isWordValid() single word pig latin only
+  if (isWordValid(word)) {
+    //if input is valid then check to see if the first letter of the word is a vowel
+    if (isFirstCharAVowel(word)) {
+      //If the first letter of a word is a vowel then output that word plus 'yay' at the end
+      return (word + 'yay').toLowerCase();
+    } else {
+      //If the first letter of a word is a consonant the output the last part of the word plus the first letter and 'ay'
+      return (word.substring(1) + word.substring(0, 1) + 'ay').toLowerCase();
+    }
+
+  } else {
+    //Tell the user to only input a single English word to turn into Pig Latin.  We aren't in the Advanced class yet!
+
+    return 'Please enter a single English word to get a word in Pig Latin. Thank you. L33t speak is 0K as well.';
+  }
 
 }
 
 
 function getPrompt() {
   rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
+    console.log(pigLatin(answer));
     getPrompt();
   });
 }
