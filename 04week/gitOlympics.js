3dd467e5a60b656a0417d6ee349e6279bd98d699
Rene Cruz <renecruzatx@outlook.com>
2018-08-02 19:35:53 -0500

    Git Olympics 1

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,12 @@
+'use strict';
+const listOfOlympians = ['Rene', 'Stefan', 'Zeke', 'Robert', 'Allan' ];
+
+
+const printListOfOlympians = (listOfOlympians) =>
+{
+  listOfOlympians.forEach((element)=>{
+    console.log(element);
+  });
+
+};
+printListOfOlympians(listOfOlympians);
